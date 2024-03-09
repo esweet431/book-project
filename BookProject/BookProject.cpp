cad81b0ad7e9330ad2dbdde1094a4b10f90eba10
Erika Sweet <ersweet@microsoft.com>
2024-03-09 14:38:30 -0800

    first commit

---
+++ BookProject/BookProject.cpp
@@ -0,0 +1,7 @@
+﻿#include "BookProject.h"
+
+int main()
+{
+	return 0;
+}
+
---
+++ BookProject/BookProject.h
@@ -0,0 +1,22 @@
+﻿
+class Product
+{
+    char* name;
+    int ISBN;
+};
+
+class ProductInventory
+{
+    public:
+        // Get the product by name
+        Product searchByName(char const* name)
+        {
+            return Product();
+        }
+
+        // Get the product by ISBN
+        Product searchByISBN(int ISBN)
+        {
+            return Product();
+        }
+};
